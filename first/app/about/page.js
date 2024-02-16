e56be977aa3136373d2cc8f56b1c8a0e81a12108
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-16 12:15:10 +0630

    useRouter() exercise

--- first/app/about/page.js
+++ first/app/about/page.js
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const About = () => {
-  return <div>About Page</div>;
+  const route = useRouter();
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={() => route.push("/")}>Go to home page.</button>
+    </div>
+  );
 };
 
 export default About;
