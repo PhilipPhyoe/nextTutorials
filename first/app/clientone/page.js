e55067f766cee8399b614a7c537d5bb549e649a3
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-16 14:46:05 +0630

    add client and server components

---
+++ first/app/clientone/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ClientOne = () => {
+  console.log("listening from client.");
+  return <div>ClientOne</div>;
+};
+
+export default ClientOne;
---
+++ first/app/serverone/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ServerOne = () => {
+  console.log("listening from server.");
+  return <div>ServerOne</div>;
+};
+
+export default ServerOne;
